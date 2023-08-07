ad4665ae719a5e484bc852fce7766c834f3f0095
xbringitback <c.muetterlein@gmail.com>
2023-08-07 09:16:31 +0200

    mail

--- backend/lib/sendMail.js
+++ backend/lib/sendMail.js
@@ -1,17 +1,14 @@
 
 
-// // const sandbox = "sandbox9762bdd2c2bc4b5ea57587dccfb2a20d.mailgun.org"
-// // const private = "9470abccea3310f74fb069af11b2710f-4e034d9e-bb56f0db"
-// // const public = "pubkey-7738bd42f93bc2b31cf113a51ba43be8"
 
 
 // import formData from "form-data";
 // import Mailgun from "mailgun.js";
 
 // const mailgun = new Mailgun(formData);
-// // const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || '9470abccea3310f74fb069af11b2710f-4e034d9e-bb56f0db'});
+// // const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || ''});
 
-// const sandbox = "sandbox9762bdd2c2bc4b5ea57587dccfb2a20d.mailgun.org"
+// const sandbox = ""
 
 // const defaultOptions = {
 //     to: ["c.muetterlein@gmail.com"],
@@ -24,7 +21,7 @@
 // export const sendMail = ({ to, subject, html } = defaultOptions) => {
 //     if (!mg) {
 //         const mg = mailgun.client({
-//             username: 'api', key: process.env.MAILGUN_API_KEY || '9470abccea3310f74fb069af11b2710f-4e034d9e-bb56f0db',
+//             username: 'api', key: process.env.MAILGUN_API_KEY || '',
 
 //         });
 //     }
